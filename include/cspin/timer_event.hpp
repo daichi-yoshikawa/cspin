3579bb09d93288cbbbd9a872e223353d51acdc42
daichi-yoshikawa <daichi.yoshikawa@gmail.com>
2020-04-14 15:13:46 -0700

    Deleted all cpp files and made cspin header-only.

--- include/cspin/timer_event.hpp
+++ include/cspin/timer_event.hpp
@@ -13,11 +13,25 @@ namespace cspin
 class TimerEvent
 {
 public:
-  explicit TimerEvent(const std::chrono::milliseconds& interval, const std::function<void()>& callback, uint32_t priority);
+  explicit TimerEvent(const std::chrono::milliseconds& interval, const std::function<void()>& callback, uint32_t priority)
+    : interval_(std::chrono::duration_cast<std::chrono::high_resolution_clock::duration>(interval)), callback_(callback), priority_(priority) {}
+    
   ~TimerEvent() { thread_.join(); }
 
-  void start();
-  void timer_event_callback();
+  void start() { thread_ = std::thread(std::bind(&TimerEvent::timer_event_callback, this)); }
+
+  void timer_event_callback()
+  {
+    using clock = std::chrono::high_resolution_clock;
+
+    while(true)
+    {
+      clock::time_point start = clock::now();
+      callback_();
+      clock::duration duration = clock::now() - start;
+      std::this_thread::sleep_for(interval_ - duration);
+    }
+  }
 
 private:
   TimerEvent(const TimerEvent&);
